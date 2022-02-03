1e2e7f57038c6786b1074bd3df58ceddec31c6a4
Mirddin Emris <anmerlin@mail.ru>
2022-02-03 13:05:04 +0300

    Вносит правки

--- src/helpers/consts.js
+++ src/helpers/consts.js
@@ -8,7 +8,7 @@ const RenderingLocation = {
   BEFORE_BEGIN: 'beforebegin',
   AFTER_BEGIN: 'afterbegin',
   BEFORE_END: 'beforeend',
-  AFTER_END: 'afterend'
+  AFTER_END: 'afterend',
 };
 
 const FormState = {
